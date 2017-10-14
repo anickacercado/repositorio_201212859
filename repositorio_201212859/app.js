8e29e630ed7caababde1aa79a243ce2dffd77c26
Anicka Michelle Cercado Figueroa <anickacercado@gmail.com>
2017-10-14 12:19:05 -0600

    Repositorio Proyecto 2 Compiladores 2

--- repositorio_201212859/app.js
+++ repositorio_201212859/app.js
@@ -6,21 +6,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+//app.set('view engine', 'pug');
+app.engine('html', ejs.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -63,3 +66,4 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
+
--- repositorio_201212859/routes/index.js
+++ repositorio_201212859/routes/index.js
@@ -1,10 +1,149 @@
 ﻿'use strict';
 var express = require('express');
+var mysql = require('mysql');
+var fs = require('fs');
 var router = express.Router();
+var usuarioActual;
+
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "admin",
+    database: "repositorio"
+});
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('login', { mensaje: "" });
 });
+
+router.get('/registrar', function (req, res) {
+    res.render('registrar', { mensaje: "" });
+});
+
+router.get('/repositorio', function (req, res) {
+    conn.connect(function (err) {
+        if (!err) {
+            console.log("Connected!");
+        }
+        var sql = "select idclase, nombre, autor, url from clase";
+        conn.query(sql, function (err, rows) {
+            if (!err) {
+                if (usuarioActual !== undefined) {
+                    console.log(rows);
+                    res.render("repositorio", { datos: rows, usr: usuarioActual });
+                }
+            }
+        });
+    });
+});
+
+router.post('/registrar', function (req, res) {
+        console.log("Entro Registrar");
+        var usuario = req.body.usuario.trim();
+        var contrasenia = req.body.contrasenia.trim();
+        var nombre = req.body.nombre.trim();
+        console.log(usuario);
+        console.log(contrasenia);
+        console.log(nombre);
+
+        conn.connect(function (err) {
+            if (!err) {
+                console.log("Connected!");
+            }
+            var sql = "insert into usuario values('" + usuario + "', '" + contrasenia + "', '" + nombre + "', 1)";
+            conn.query(sql, function (err, result) {
+                if (!err) {
+                    res.render('login', {mensaje: "Usuario se ha registrado" });
+                }
+                else {
+                    res.render('registrar',{mensaje: "Usuario ya Existe"});
+                }
+            });
+        });
+});
+
+router.post('/', function (req, res) {
+    console.log("Entro Registrar");
+    var usuario = req.body.usuario.trim();
+    var contrasenia = req.body.contrasenia.trim();
+    console.log(usuario);
+    console.log(contrasenia);
+    usuarioActual = undefined;
+
+
+    conn.connect(function (err) {
+        if (!err) {
+            console.log("Connected!");
+        }
+        var sql = "select * from usuario where usuario= '" + usuario + "' and contrasenia= '" + contrasenia + "'";
+        conn.query(sql, function (err, rows) {
+            if (!err) {
+                if (rows.length > 0) {
+                    usuarioActual = usuario;
+                    res.redirect('/repositorio');
+                }
+                else {
+                    res.render('login', { mensaje: "Usuario - Contraseña no Coinciden" });
+                }
+            }
+        });
+    });
+});
+
+
+router.get('/eliminar', function (req, res) {
+    var id = req.query['id'];    
+    conn.connect(function (err) {
+        if (!err) {
+            console.log("Connected!");
+        }
+        var sql = "delete from clase where idclase = " + id;
+        conn.query(sql, function (err, result) {
+            if (!err) {
+                res.redirect('/repositorio');
+            }
+            else {
+                console.log(err);
+                return;
+            }
+        });
+    });
+});
+
+router.get('/clase', function (req, res) {
+    var id = req.query['id'];
+    console.log("Entra Clase");
+    console.log(id);
+
+    conn.connect(function (err) {
+        if (!err) {
+            console.log("Connected!");
+        }
+        var sql = "select * from clase where idclase= " + id;
+        conn.query(sql, function (err, rows) {
+            if (!err) {
+                console.log(rows);
+                res.render("clase.html", { datos: rows });
+            }
+        });
+    });
+});
+
+
+router.post('/clase', function (req, res) {
+    var nombre = req.body.nom;
+    var codigo = req.body.cod;
+    console.log(nombre);
+    console.log(codigo);
+
+    fs.writeFile("./" + nombre, codigo, function (err) {
+        if (err) {
+            return console.log(err);//si el archivo no se pudo guardar
+        }
+    });
+});
+
 
 module.exports = router;
